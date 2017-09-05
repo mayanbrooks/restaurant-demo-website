40e71e500d01be56c7cf650d62965258b63c3ea5
Maya Noelle <maya.n.brooks@gmail.com>
2017-09-05 02:34:36 -0400

    First Commit.

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+      $('.parallax').parallax();
+    });
+
+$(document).ready(function(){
+    $('.carousel').carousel({duration: 200});
+    });
+
+
+$('.carousel.carousel-slider').carousel({fullWidth: true}, {noWrap:false});
